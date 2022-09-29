ad2b1c761e60719b6353e32f6a94a5c257c20cc1
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2022-09-29 10:27:20 -0400

    Added some extra clean-up

--- alpha/front/midlogin.php
+++ alpha/front/midlogin.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
     $username = $_POST['username'];
     $password = $_POST['password'];
-    // Url to change later
+    // url to change later
     $url = "https://afsaccess4.njit.edu/~nk553/CS490/backend.php";
     // Fields array to collect the previous information
     $fields = [
@@ -30,3 +30,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Send back the full response - json as opposed to a string
     echo $response;
   }
+?>
