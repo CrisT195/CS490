a2add153cab13442879c6c2b6c0706931bcd4a25
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2022-10-17 16:00:38 -0400

    Updated grade to be almost done for now

--- beta/middle/grade.php
+++ beta/middle/grade.php
@@ -4,9 +4,9 @@
 // if ($_SERVER["REQUEST_METHOD"] == "POST") {
 //     // collect value of input field
 //     $response = $_POST['response'];
-$questions = json_decode('[["unpublished","student300","16","def hi():\n\treturn 0",null,null,null]]');
-print_r($questions);
-echo count($questions);
+$decoded_data = json_decode('[["unpublished","student300","16","def hi():\n\treturn 0",null,null,null]]');
+// print_r($decoded_data);
+// echo count($decoded_data);
 // It takes json for all the student responses and
 // returns a string for the output of the student's answers
 
@@ -16,87 +16,129 @@ echo count($questions);
 // You'll need to decode the json
 // and use the questions length to do the loop
 // and use the json values
-// $questionindex = 0;
-// $questions = 3;
-// $pointsarray = [0];
-// while ($questionindex < $questions) {
-//     // Get the total number of points and take away some points to reconstruct it later
-//     $studentpoints = 0;
-//     $totalpoints = 20;
-//     $correctnamepoints = 5;
-//     $totalpoints -= $correctnamepoints;
-//     // Update correct function (testcase) name and the full student response string
-//     $i = 0;
-//     // Get the test case name to determine what the true name of the function is
-//     $testcases = ["hi() == 0", "hi() == 0"];
+$questionindex = 0;
+$questions = count($decoded_data);
+$pointsarray = [0];
+while ($questionindex < $questions) {
+    $usernameindex = 1;
+    $questionidindex = 2;
+    $username = $decoded_data[$questionindex][$usernameindex];
+    $questionid = $decoded_data[$questionindex][$questionidindex];
+// Turn into imported function
+// url to change later
+$url = "https://afsaccess4.njit.edu/~bjb38/CS490/fullexaminfo.php";
+// Fields array to collect the previous information
+$fields = [
+    "username" => $username,
+    "questionid" => $questionid
+];
+// Builds into a string from the array
+$fields_string = http_build_query($fields);
+// create a new cURL resource
+$ch = curl_init();
 
-//     $testcasepoints = $totalpoints / count($testcases);
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
 
-//     $testcasepos = [0];
-//     foreach($testcases as $testcase) {
-//         $testcasepos[$i] = strpos($testcase, "(");
-//         $i++;
-//     }
-//     $correctfuncname = substr($testcases[0], 0, $testcasepos[0]);
-//     // Check if the name of the response is the same as real name
-//     $responsestr = "def hi():\n\treturn 0";
-//     $spacepos = strpos($responsestr, " ");
-//     $namepos = $spacepos + 1;
-//     $parenpos = strpos($responsestr, "(");
-//     $namelength = $parenpos - $namepos;
-//     $extractedfuncname = substr($responsestr, $namepos, $namelength);
-//     if ($extractedfuncname != $correctfuncname) {
-//         // Unequal
-//         echo 'Both strings are not equal';
-//         $responsestr = str_replace($extractedfuncname, $correctfuncname, $responsestr);
-//         echo "<br>".$responsestr;
-//         // Deduct 5 points
-//     } else {
-//         echo 'Both strings are equal';
-//         // Equal
-//         // Add back the points we took
-//         $studentpoints += $correctnamepoints;
-//     }
+// grab URL and pass it to the browser
+$response = curl_exec($ch);
 
-//     // Take this echo out
-//     echo "<br>";
+// close cURL resource, and free up system resources
+curl_close($ch);
+// echo $response;
+$fullexam = json_decode($response, true);
+// print_r($fullexam);
+// echo $fullexam['status'];
+// echo $totalpoints;
 
-//     $myfile = fopen("grade.py", "w") or die("Unable to open file!");
-//     // Write the python file with student response and test cases with newlines
-//     $functxt = $responsestr;
-//     $nl = "\n";
-//     fwrite($myfile, $functxt);
-//     fwrite($myfile, $nl);
-//     $testcasestxt = ["print({$testcases[0]})", "print({$testcases[1]})"];
-//     fwrite($myfile, $testcasestxt[0]);
-//     fwrite($myfile, $nl);
-//     fwrite($myfile, $testcasestxt[1]);
-//     fclose($myfile);
+// Import end
+    // Get the total number of points and take away some points to reconstruct it later
+    $studentpoints = 0;
+    $totalpoints = $fullexam["pnts"];
+    $correctnamepoints = 5;
+    $totalpoints -= $correctnamepoints;
+    // Update correct function (testcase) name and the full student response string
+    $i = 0;
+    // Get the test case name to determine what the true name of the function is
+    $testcases = [$fullexam["testcase1"], $fullexam["testcase2"]];
+    $outputs = [$fullexam["output1"], $fullexam["output2"]];
 
-//     $command = 'python grade.py';
-//     $out = null;
-//     $status = null;
-//     $command = escapeshellcmd($command);
-//     exec($command, $out, $status);
-//     foreach($out as $answer) {
-//         // False and True evaluate to True in php,
-//         // so this converts it to real true/false
-//         if (filter_var($answer, FILTER_VALIDATE_BOOLEAN)) {
-//             $studentpoints += $testcasepoints;
-//         }
-//     }
-//     print_r($out);
-//     echo $status;
-//     // Take this echo out
-//     echo "<br>";
-//     $pointsarray[$questionindex] = $studentpoints;
-//     echo $studentpoints;
-//     // Go to next question
-//     $questionindex++;
-// }
-// // Take this echo out
-// echo "<br>";
-// print_r($pointsarray); 
+    $testcasepoints = $totalpoints / count($testcases);
+
+    $testcasepos = [0];
+    foreach($testcases as $testcase) {
+        $testcasepos[$i] = strpos($testcase, "(");
+        $i++;
+    }
+    $correctfuncname = substr($testcases[0], 0, $testcasepos[0]);
+    // Check if the name of the response is the same as real name
+    // $responsestr = $fullexam["answer"];
+    $responsestr = "def hi(a):\n\treturn 0";
+    echo $responsestr;
+    $spacepos = strpos($responsestr, " ");
+    $namepos = $spacepos + 1;
+    $parenpos = strpos($responsestr, "(");
+    $namelength = $parenpos - $namepos;
+    $extractedfuncname = substr($responsestr, $namepos, $namelength);
+    if ($extractedfuncname != $correctfuncname) {
+        // Unequal name
+        echo 'Both strings are not equal';
+        $responsestr = str_replace($extractedfuncname, $correctfuncname, $responsestr);
+        echo "<br>".$responsestr;
+    } else {
+        echo 'Both strings are equal';
+        // Equal name
+        // Add back the points we took
+        $studentpoints += $correctnamepoints;
+    }
+
+    // Take this echo out
+    echo "<br>";
+
+    $myfile = fopen("grade.py", "w") or die("Unable to open file!");
+    // Write the python file with student response and test cases with newlines
+    $functxt = $responsestr;
+    $nl = "\n";
+    fwrite($myfile, $functxt);
+    fwrite($myfile, $nl);
+    $testcasestxt = ["print({$testcases[0]})", "print({$testcases[1]})"];
+    fwrite($myfile, $testcasestxt[0]);
+    fwrite($myfile, $nl);
+    fwrite($myfile, $testcasestxt[1]);
+    fclose($myfile);
+
+    $command = 'python grade.py';
+    $out = null;
+    $status = null;
+    $command = escapeshellcmd($command);
+    exec($command, $out, $status);
+    print_r($out);
+    print_r($outputs);
+    $out_i = 0;
+    foreach($out as $answer) {
+        // False and True evaluate to True in php,
+        // so this converts it to real true/false
+        // echo $answer."<br>";
+        // Maybe convert both to filter var
+        if (filter_var($answer, FILTER_VALIDATE_BOOLEAN) == $outputs[$out_i]) {
+            $studentpoints += $testcasepoints;
+        }
+        $out_i++;
+    }
+    // echo $status;
+    // Take this echo out
+    echo "<br>";
+    $pointsarray[$questionindex] = $studentpoints;
+    echo $studentpoints;
+    // Go to next question
+    $questionindex++;
+}
+// Take this echo out
+echo "<br>";
+print_r($pointsarray); 
 // }
 // Send response using curl to update
 ?>
