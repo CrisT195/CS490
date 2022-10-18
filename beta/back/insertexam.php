8617a9594be3b207fcd7c5c24cbb3cd656de9035
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2022-10-18 19:44:24 -0400

    Added easier examnum for insertexam

--- beta/back/insertexam.php
+++ beta/back/insertexam.php
@@ -15,6 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     }
 
     $questions = $_POST['questions'];
+    $examnum = $_POST['examnum'];
     $decoded_questions = json_decode($questions, true);
     $examdata = $decoded_questions["exam"];
     for ($i = 0;$i < count($examdata);$i++)
@@ -22,49 +23,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         $examquestion = $examdata[$i];
         $qid = $examquestion[0];
         $pointstr = $examquestion[1];
-
-        // This block gets the correct examnum
-        $sql12 = "SELECT * FROM Exam";
-        $result = $connect->query($sql12);
-        if ($result->num_rows < 3)
-        {
-            // Examnum will always start at 1 when there are less than 3 elements
-            $sql4 = "INSERT INTO Exam (questionId, pnts, examnum)
-            VALUES
-            ('$qid', '$pointstr', '1')";
-            $connect->query($sql4);
-        }
-        else if ($result->num_rows >= 3)
-        {
-            // This will return the highest id first
-            $sqlc1 = "SELECT * FROM Exam ORDER BY id DESC LIMIT 3";
-            $examarr = $connect->query($sqlc1);
-            $rows = $examarr->fetch_all();
-            // Examnumindex is the current index the examnum is at in the database
-            $examnumindex = 3;
-            if ($rows[0][$examnumindex] == $rows[1][$examnumindex] && $rows[1][$examnumindex] == $rows[2][$examnumindex])
-            {
-                // All equal, so the examnum is all the same, create a new one
-                $examnum = $rows[0][$examnumindex] + 1;
-                $sql4 = "INSERT INTO Exam (questionId, pnts, examnum)
-               VALUES
-               ('$qid', '$pointstr', '$examnum')";
-                $connect->query($sql4);
-
-            }
-            // They are not the same, so the exam num is still not done
-            else if ($rows[0][$examnumindex] != $rows[1][$examnumindex] || $rows[1][$examnumindex] != $rows[2][$examnumindex])
-            {
-
-                // Examnum is not the same, so we still have a bit more to insert
-                $examnum = $rows[0][$examnumindex];
-                $sql4 = "INSERT INTO Exam (questionId, pnts, examnum)
-               VALUES
-               ('$qid', '$pointstr', '$examnum')";
-                $connect->query($sql4);
-
-            }
-        }
+        $sql4 = "INSERT INTO Exam (questionId, pnts, examnum)
+        VALUES
+        ('$qid', '$pointstr', '$examnum')";
+        $connect->query($sql4);
     }
     $connect->close();
 }
