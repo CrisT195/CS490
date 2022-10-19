50fd53048083a8ac09ec6d1b66365b936acd11b4
Cristina Hilario <62902433+CrisT195@users.noreply.github.com>
2022-10-19 01:23:55 -0400

    Create takeexam.php

---
+++ beta/front/student/takeexam.php
@@ -0,0 +1,86 @@
+<?php
+ session_start();
+ if(!(isset($_SESSION["user"]) && isset($_SESSION["user"]["role"]) && $_SESSION["user"]["role"] == "student")) {
+  die(header("Location: ../login.php"));
+ }
+?>
+
+<!DOCTYPE html> 
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exam in-progress</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+  </head>
+  <body>
+   <div class="container">
+    <h3>Exam in progress</h3>
+    <div id="examquestions"></div>
+    <input id="submitexam" type="submit" name="submit" value="Submit Exam" class="btn btn-primary"/>
+   </div>
+
+   <script>
+    var ex = [localStorage['exam']];
+    localStorage.removeItem('exam'); // clear localStorage
+    console.log("exam name: " + ex);
+    fetch("https://afsaccess4.njit.edu/~cth9/CS490/student/getexamquestions.php", {
+     method: "POST",
+     headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+     },
+     body: JSON.stringify(ex)
+    }).then((res) => {
+     return res.json();
+    }).then((data) => {
+    // [["published","student300","16","def hi(a):\\n\\treturn 0","5,5,5","Good",null]]
+    // [[status, username, questionid, question, pnts, comments], ..]
+    console.log(data); // [{question:'', pnts: '15', questionId: '20', etc.}, {…}, {…}]
+
+
+    // exam
+    if (data.length < 1) {
+     window.location.href='studenthome.php';
+     return true;
+    }
+    var exam = "";
+    for (var i in data) {
+     var ques = data[i]["question"];
+     exam += '<br><div><h3>'+i+". " + ques + '&emsp;<b> (' + data[i]["pnts"] + ' points)</b></h3><textarea id="textarea'+ i +'"></textarea>' + '</div><br>';
+    }
+    document.getElementById("examquestions").innerHTML = exam;
+
+    $("textarea").keydown(function(e) {
+     if(e.keyCode === 9) { // tab was pressed
+      // get caret position/selection
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
+
+      var $this = $(this);
+      var value = $this.val();
+
+      // set textarea value to: text before caret + tab + text after caret
+      $this.val(value.substring(0, start) + "\t" + value.substring(end));
+
+      // put caret at right position again (add one for the tab)
+      this.selectionStart = this.selectionEnd = start + 1;
+
+      // prevent the focus lose
+      e.preventDefault();
+     }
+    });
+
+    document.getElementById("submitexam").addEventListener('click', function(e){
+     console.log("clicked submit exam");
+     var temp = document.getElementById("textarea0").value;
+     console.log(temp);
+     // TODO: translate to \t and \n
+    });
+
+   });
+   </script>
+  </body>
+</html>
