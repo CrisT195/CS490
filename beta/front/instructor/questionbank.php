768540763bac2136e7bec4a1a46503aee3477591
Cristina Hilario <62902433+CrisT195@users.noreply.github.com>
2022-10-19 01:14:22 -0400

    replace placeholder data with dynamic data

--- beta/front/instructor/questionbank.php
+++ beta/front/instructor/questionbank.php
@@ -1,24 +1,21 @@
 <?php
-// currently placeholder data. Working version will dynamically get questionbank from middle end.
+session_start();
+if(!(isset($_SESSION["user"]) && isset($_SESSION["user"]["role"]) && $_SESSION["user"]["role"] == "teacher")) {
+  die(header("Location: ../login.php"));
+}
+?>
 
-$questions = array();
-$questions["questions"][0]["id"] = 0;
-$questions["questions"][0]["description"] = "Given a string name, e.g. 'Bob', create a function named 'hello_name' that returns a greeting of the form 'Hello Bob!'.";
-$questions["questions"][0]["difficulty"] = "easy";
-$questions["questions"][0]["category"] = "strings";
+<?php
+// Asks middle for question bank, and returns to front
 
-$questions["questions"][1]["id"] = 1;
-$questions["questions"][1]["description"] = "question 2";
-$questions["questions"][1]["difficulty"] = "medium";
-$questions["questions"][1]["category"] = "loops";
+$url = 'https://afsaccess4.njit.edu/~bjb38/CS490/midallquestions.php';
+$ch = curl_init($url); 
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result;
 
-$questions["questions"][2]["id"] = 2;
-$questions["questions"][2]["description"] = "question 3";
-$questions["questions"][2]["difficulty"] = "hard";
-$questions["questions"][2]["category"] = "arithmetic";
-
-$json = json_encode($questions);
-echo $json;
 
 // ex: { "questions" : [ {"id":0, "description":"Given a string name, e.g. 'Bob', create a function named 'hello_name' that returns a greeting of the form 'Hello Bob!'." , "difficulty":"easy", "category":"strings" }, {"id":1, "description":"question 2" , "difficulty":"medium", "category":"loops" }, {"id":2, "description":"question 3" , "difficulty":"hard", "category":"arithmetic" } ] }
 ?>
